f673342138474fc00d237fff9a13b1b318b3923b
Onyerikam <115038141+Onyerikam@users.noreply.github.com>
2023-01-02 08:49:48 +0000

    code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,208 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { blockList } from 'block-list'; // library for blocking ads by provider
+import { Toggle, Indicator, Themes, Chart, Form, Settings, Feedback, Support, Language, Search, Updater, Extension } from 'material-ui'; // Material-UI components for toggle buttons, indicators, themes, charts, forms, settings, feedback, support, language, search, updater, and extension
+import { Tour } from 'react-tour'; // library for creating guided tours
+import { Drive, Dropbox } from 'cloud-storage'; // cloud storage libraries
+import { Notification } from 'notification'; // library for displaying notifications
+import { IntlProvider } from 'react-intl'; // library for providing localization support
+import D3 from 'd3'; // library for creating charts
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class AdBlocker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user,
+      blockedAds: [],
+      whitelist: [],
+      blacklist: [],
+      adStats: {
+        totalBlocked: 0,
+        timeSaved: 0,
+        dataSaved: 0
+      },
+      blockingSettings: {
+        adTypes: ['popup', 'video', 'audio'],
+        maxAds: 3
+      },
+      adHistory: [],
+      adBlockerEnabled: true,
+      theme: 'light'
+    };
+  }
+
+  blockAd(ad) {
+    this.setState({
+      blockedAds: [...this.state.blockedAds, ad],
+      adStats: {
+        ...this.state.adStats,
+        totalBlocked: this.state.adStats.totalBlocked + 1
+      },
+      adHistory: [...this.state.adHistory, ad]
+    });
+  }
+
+  blockAdsFromProvider(provider) {
+    blockList.block(provider);
+  }
+
+  reportAd(ad) {
+    // send ad report to server for review
+  }
+
+  addToWhitelist(url) {
+    this.setState({
+      whitelist: [...this.state.whitelist, url]
+    });
+  }
+
+  addToBlacklist(url) {
+    this.setState({
+      blacklist: [...this.state.blacklist, url]
+    });
+  }
+
+  updateBlockingSettings(settings) {
+    this.setState({
+      blockingSettings: {
+        ...this.state.blockingSettings,
+        ...settings
+      }
+    });
+  }
+
+  toggleAdBlocker(enabled) {
+    this.setState({
+      adBlockerEnabled: enabled
+    });
+  }
+
+  selectTheme(theme) {
+    this.setState({
+      theme: theme
+    });
+  }
+
+  syncWithCloud(service) {
+    if (service === 'drive') {
+      Drive.sync(this.state);
+    } else if (service === 'dropbox') {
+      Dropbox.sync(this.state);
+    }
+  }
+
+  importData(file) {
+    const data = readFile(file);
+    this.setState({
+      ...data
+    });
+  
+    exportData() {
+      const data = this.state;
+      const file = createFile(data);
+      downloadFile(file);
+    }
+  
+    displayNotification(notification) {
+      Notification.show(notification);
+    }
+  
+    displayAdStatistics() {
+      const data = this.state.adStats;
+      const chart = D3.createChart(data);
+      return <Chart data={chart} />;
+    }
+  
+    displayBlockingSettingsForm() {
+      const formData = this.state.blockingSettings;
+      return <Form data={formData} onSubmit={this.updateBlockingSettings} />;
+    }
+  
+    displayToggle() {
+      const enabled = this.state.adBlockerEnabled;
+      return <Toggle checked={enabled} onChange={this.toggleAdBlocker} />;
+    }
+  
+    displayThemeSelector() {
+      const theme = this.state.theme;
+      return <Themes value={theme} onChange={this.selectTheme} />;
+    }
+  
+    displaySyncIndicator() {
+      const syncing = this.state.syncing;
+      return <Indicator active={syncing} />;
+    }
+  
+    displayAdHistory() {
+      const history = this.state.adHistory;
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>URL</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {history.map((ad) => (
+              <tr key={ad.url}>
+                <td>{ad.url}</td>
+                <td>{ad.type}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
+    }
+  
+    displayWhitelist() {
+      const whitelist = this.state.whitelist;
+      return (
+        <ul>
+          {whitelist.map((url) => (
+            <li key={url}>
+              {url}
+              <button onClick={() => this.removeFromWhitelist(url)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  
+    displayBlacklist() {
+      const blacklist = this.state.blacklist;
+      return (
+        <ul>
+          {blacklist.map((url) => (
+            <li key={url}>
+              {url}
+              <button onClick={() => this.removeFromBlacklist(url)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  
+    displayCloudSyncOptions() {
+      return (
+        <div>
+          <button onClick={() => this.syncWithCloud('drive')}>Sync with Google Drive</button>
+          <button onClick={() => this.syncWithCloud('dropbox')}>Sync with Dropbox</button>
+        </div>
+      );
+    }
+  
+    displayImportExportOptions() {
+      return (
+        <div>
+          <input type="file" onChange={(event) => this.importData(event.target.files [0])} />
+          <button onClick={() => this.exportData()}>Export Data</button>
+      </div>
+    );
+  }
+
+  displayAdBlockerStatus() {
+    const enabled = this.state.adBlockerEnabled;
+    return <Indicator active={enabled} />;
+  }
 }
-
-export default App;
+}
